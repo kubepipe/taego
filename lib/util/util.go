f428ada863112d3ba01bb05b8a787177af141a2c
laotoutou <zhihao.huang.1997@gmail.com>
2022-09-18 10:18:00 +0800

    clean utils funcs

--- lib/util/util.go
+++ lib/util/util.go
@@ -2,11 +2,8 @@ package util
 
 import (
 	"os"
-	"strconv"
 
 	"taego/mconst"
-
-	"github.com/gin-gonic/gin"
 )
 
 func GetMode() mconst.MODE {
@@ -15,12 +12,4 @@ func GetMode() mconst.MODE {
 		mode = string(mconst.MODE_DEBUG)
 	}
 	return mconst.MODE(mode)
-}
-
-func QueryInt(c *gin.Context, key string) int {
-	if c == nil {
-		return 0
-	}
-	v, _ := strconv.Atoi(c.Query(key))
-	return v
 }
